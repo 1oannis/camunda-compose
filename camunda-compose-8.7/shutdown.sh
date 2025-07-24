c263f6d4ae36369ee75e004174e577ac8269c572
1oannis <thio1011@h-ka.de>
2025-07-24 00:26:01 +0000

    externalize envs

---
+++ camunda-compose-8.7/shutdown.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker compose --env-file .env \
+  --env-file configuration/runtime.env \
+  --env-file configuration/runtime_local_${HOSTNAME:-SAMPLEHOST}.env \
+  --env-file configuration/runtime_local_${HOSTNAME:-SAMPLEHOST}/base-infrastructure.env \
+  --env-file configuration/runtime_local_${HOSTNAME:-SAMPLEHOST}/identity.env \
+  --env-file configuration/runtime_local_${HOSTNAME:-SAMPLEHOST}/core-services.env \
+  --env-file configuration/runtime_local_${HOSTNAME:-SAMPLEHOST}/web-modeler.env \
+  down -v
---
+++ camunda-compose-8.7/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./directory_creation_util.sh
+
+docker compose --env-file .env \
+  --env-file configuration/runtime.env \
+  --env-file configuration/runtime_local_${HOSTNAME:-SAMPLEHOST}.env \
+  --env-file configuration/runtime_local_${HOSTNAME:-SAMPLEHOST}/base-infrastructure.env \
+  --env-file configuration/runtime_local_${HOSTNAME:-SAMPLEHOST}/identity.env \
+  --env-file configuration/runtime_local_${HOSTNAME:-SAMPLEHOST}/core-services.env \
+  --env-file configuration/runtime_local_${HOSTNAME:-SAMPLEHOST}/web-modeler.env \
+  up -d
